a9b13a0fe772ad6f95e9b544765219bc58dfde79
fogrexon <fogrexon@gmail.com>
2020-05-23 13:38:01 +0900

    Geometry TS化

--- src/geometry/Geometry.d.ts
+++
@@ -1,8 +0,0 @@
-export class Geometry
-{
-  constructor(vertex: number[], normal: number[], uv: number[], index: number[]);
-
-  setupAttribute(gl: WebGLRenderingContext, program: WebGLProgram): void;
-
-  attachAttribute(gl: WebGLRenderingContext): void;
-}
