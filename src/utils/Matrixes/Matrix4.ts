32314e013145c58bae3e2ba839ed2d79f8f7d670
fogrexon <fogrexon@gmail.com>
2020-05-25 18:46:14 +0900

    inverse

--- src/utils/Matrixes/Matrix4.ts
+++ src/utils/Matrixes/Matrix4.ts
@@ -109,6 +109,52 @@ class Matrix4 {
       m[12] * mul, m[13] * mul, m[14] * mul, m[15] * mul,
     ]);
   }
+
+  transpose(): Matrix4 {
+    const m: number[] = this.matrix;
+    return new Matrix4([
+      m[0], m[4], m[8], m[12],
+      m[1], m[5], m[9], m[13],
+      m[2], m[6], m[10], m[14],
+      m[3], m[7], m[11], m[15],
+    ]);
+  }
+
+  inverse(): Matrix4 {
+    const mat: number[] = this.matrix;
+    const a = mat[0]; const b = mat[1]; const c = mat[2]; const d = mat[3];
+    const e = mat[4]; const f = mat[5]; const g = mat[6]; const h = mat[7];
+    const i = mat[8]; const j = mat[9]; const k = mat[10]; const l = mat[11];
+    const m = mat[12]; const n = mat[13]; const o = mat[14]; const p = mat[15];
+    const q = a * f - b * e; const r = a * g - c * e;
+    const s = a * h - d * e; const t = b * g - c * f;
+    const u = b * h - d * f; const v = c * h - d * g;
+    const w = i * n - j * m; const x = i * o - k * m;
+    const y = i * p - l * m; const z = j * o - k * n;
+    const A = j * p - l * n; const B = k * p - l * o;
+    let ivd = (q * B - r * A + s * z + t * y - u * x + v * w);
+    if (ivd === 0) throw new Error('det A == 0');
+    ivd = 1 / ivd;
+
+    const dest: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    dest[0] = (f * B - g * A + h * z) * ivd;
+    dest[1] = (-b * B + c * A - d * z) * ivd;
+    dest[2] = (n * v - o * u + p * t) * ivd;
+    dest[3] = (-j * v + k * u - l * t) * ivd;
+    dest[4] = (-e * B + g * y - h * x) * ivd;
+    dest[5] = (a * B - c * y + d * x) * ivd;
+    dest[6] = (-m * v + o * s - p * r) * ivd;
+    dest[7] = (i * v - k * s + l * r) * ivd;
+    dest[8] = (e * A - f * y + h * w) * ivd;
+    dest[9] = (-a * A + b * y - d * w) * ivd;
+    dest[10] = (m * u - n * s + p * q) * ivd;
+    dest[11] = (-i * u + j * s - l * q) * ivd;
+    dest[12] = (-e * z + f * x - g * w) * ivd;
+    dest[13] = (a * z - b * x + c * w) * ivd;
+    dest[14] = (-m * t + n * r - o * q) * ivd;
+    dest[15] = (i * t - j * r + k * q) * ivd;
+    return new Matrix4(dest);
+  }
 }
 
 export { Matrix4 };
